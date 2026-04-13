a505889e0d1ff8fd354cb8aeb2c5115b78582d70
Bhavish-ba6850 <ba6850@srmist.edu.in>
2026-04-13 16:32:03 +0530

    UC:18 - Linear Search for Bogie ID (Array-Based Searching)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -2,16 +2,27 @@
 import java.util.Arrays;
 public class TrainConsistManagementApp {
 
-
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
 
-        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
-        Arrays.sort(bogieNames);
+        String searchKey = "BG309";
 
-        System.out.println("\nSorted Bogie Names:");
-        System.out.println(Arrays.toString(bogieNames));
+        boolean found = false;
+
+        for (String id : bogieIds) {
+            if (id.equals(searchKey)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Bogie ID " + searchKey + " exists in the consist.");
+        } else {
+            System.out.println("Bogie ID " + searchKey + " not found.");
+        }
     }
 }
